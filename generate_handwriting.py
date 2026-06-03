809fdac55c1d353cf73cd80aecdb9724b03eb2cd
Zit87 <144679925+Zit87@users.noreply.github.com>
2026-06-03 19:16:18 +0300

    Increase bias to 5.0 for better handwriting quality

--- generate_handwriting.py
+++ generate_handwriting.py
@@ -5,22 +5,22 @@ from model import HandwritingGenerator
 from utils import *
 
 
-MODEL_PATH = '/content/drive/MyDrive/Handwriting/model.pth'
-train_dataset = GetDataset('/content/drive/MyDrive/Handwriting', split = 'train')
-test_dataset = GetDataset('/content/drive/MyDrive/Handwriting', split = 'test')
-model = HandwritingGenerator(vocab_size = train_dataset.vocab_size, hidden_size = 400, num_layers = 3, num_mixtures_attn = 10, num_mixtures_output = 20).cuda()
-model.load_state_dict(torch.load(MODEL_PATH))
+MODEL_PATH = './model.pth'
+train_dataset = GetDataset('.', split = 'train')
+test_dataset = GetDataset('.', split = 'test')
+model = HandwritingGenerator(vocab_size = train_dataset.vocab_size, hidden_size = 400, num_layers = 3, num_mixtures_attn = 10, num_mixtures_output = 20).cpu()
+model.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
 
 
 while True:
     string = input("Enter input: ") + " "
     chars = torch.from_numpy(
         test_dataset.sentence2idx(string)
-    ).long()[None].cuda()
-    chars_mask = torch.ones_like(chars).float().cuda()
+    ).long()[None].cpu()
+    chars_mask = torch.ones_like(chars).float().cpu()
 
     with torch.no_grad():
-        out = model.sample(chars, chars_mask, bias = 2.0)[0].cpu().numpy()
+        out = model.sample(chars, chars_mask, bias = 5.0)[0].cpu().numpy()
 
     draw(out[0], save_file='./generated.jpg')
     plt.imshow(out[0])
